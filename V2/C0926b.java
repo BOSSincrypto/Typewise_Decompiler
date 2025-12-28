4471753fb635e1e02ba454915fc9c1ddd8558a4f
BOSSincrypto <104099876+BOSSincrypto@users.noreply.github.com>
2025-12-28 16:08:58 +0300

    Delete V2 directory

--- V2/C0926b.java
+++
@@ -1,5 +0,0 @@
-package v2;
-
-/* renamed from: v2.b  reason: case insensitive filesystem */
-public interface C0926b extends C0925a {
-}
--- V2/c.java
+++
@@ -1,4 +0,0 @@
-package v2;
-
-public interface c extends C0926b {
-}
