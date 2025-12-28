eb88df840449effa361a7dc6861b553f9bb90610
BOSSincrypto <104099876+BOSSincrypto@users.noreply.github.com>
2025-12-28 16:08:49 +0300

    Delete V1 directory

--- V1/f.java
+++
@@ -1,8 +0,0 @@
-package V1;
-
-import android.os.Bundle;
-import android.os.IInterface;
-
-public interface f extends IInterface {
-    void b(String str, Bundle bundle, h hVar);
-}
--- V1/h.java
+++
@@ -1,8 +0,0 @@
-package V1;
-
-import android.os.Bundle;
-import android.os.IInterface;
-
-public interface h extends IInterface {
-    void e(Bundle bundle);
-}
